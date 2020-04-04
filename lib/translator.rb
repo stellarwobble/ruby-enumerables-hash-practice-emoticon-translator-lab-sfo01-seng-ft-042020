6683b1e62eb337a29e3c743f393e9a23a7554c06
Jennifer Paquette <jennidemuir@gmail.com>
2020-04-04 18:54:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,11 +27,10 @@ end
 
 def get_english_meaning(path, emoticon)
  emoticons = load_library(path)
- result = emoticons["get_meaning"][emoticon]
- if result
-   result
- else
-   "Sorry, that emoticon was not found"
- end
-
+  result = emoticons["get_meaning"][emoticon] #the same above but change get_meaning
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
